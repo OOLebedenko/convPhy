478bcec4faf272944246e8e1bc4b9104c1e216c5
OOLebedenko <oolebedenko@gmail.com>
2019-06-21 16:04:47 +0300

    Add flags for performed steps of convPhy pipeline

--- general_part.py
+++ general_part.py
@@ -6,6 +6,11 @@ from scripts.annotation.annotate_snp import annotate_snp
 parser = argparse.ArgumentParser(description='search SNP ')
 parser.add_argument("-i", "--input", required=True, help="path to dir with of all input files")
 parser.add_argument("-o", "--out_dir", default="./output_phyc/", help="path to dir_output")
+parser.add_argument("-geno", "--genotype_prediction", default=False, type=bool)
+parser.add_argument("-pheno", "--phenotype_prediction", default=False, type=bool)
+parser.add_argument("-conphy", default=False, type=bool)
+parser.add_argument("-p_value", default=False, type=bool)
+
 
 args = parser.parse_args()
 # # входные файлы
@@ -24,22 +29,26 @@ os.makedirs(out_dir, exist_ok=True)
 os.chdir(run_dir)
 
 # Подготовка файлов и запуск raxml
-path_to_anc_phy = run_raxml(out_dir, raxml_in, phylip_in)
-path_to_anc_phy = os.path.join(out_dir, 'raxml', 'RAxML_marginalAncestralStates.nh') #TODO: Raxml??
+if not args.geno:
+    path_to_anc_phy = run_raxml(out_dir, raxml_in, phylip_in)
+path_to_anc_phy = os.path.join(out_dir, 'raxml', 'RAxML_marginalAncestralStates.nh')
 
 # Подготовка файлов и запуск phenotype_prediction.py
-name_of_R, name_of_S, names_of_ancestral_S, names_of_ancestral_R, genotype = \
-    run_phenotype_prediction(out_dir, phylip_in, path_to_anc_phy, R_in, S_in)
+if not args.pheno:
+    name_of_R, name_of_S, names_of_ancestral_S, names_of_ancestral_R, genotype = \
+        run_phenotype_prediction(out_dir, phylip_in, path_to_anc_phy, R_in, S_in)
 
 # Подготовка файлов и запуск phyC
-R_S = run_phyc(out_dir, name_of_R, name_of_S, names_of_ancestral_S,
+if not args.phyc:
+    R_S = run_phyc(out_dir, name_of_R, name_of_S, names_of_ancestral_S,
          names_of_ancestral_R, info_pos, genotype) #TODO: change on ancestral labeled tree
 
 # Подготовка файлов и запуск p_value
 out_dir = os.path.join(args.out_dir, "p_value")
-with open(os.path.join(args.out_dir, "phyc", "pos.txt")) as f_in:
-	R_S = [line.strip().split() for line in f_in]
-run_p_value(out_dir, R_S, info_pos)
+if not args.p_vaue:
+    with open(os.path.join(args.out_dir, "phyc", "pos.txt")) as f_in:
+        R_S = [line.strip().split() for line in f_in]
+    run_p_value(out_dir, R_S, info_pos)
 
 # Аннотация ОНП
 
