ecdc396c35e692c526963fa7b912b3b6a16c5adb
OOLebedenko <oolebedenko@gmail.com>
2019-08-24 18:03:21 +0300

    Add create_input.py in main part

---
+++ create_input.py
@@ -0,0 +1,16 @@
+from scripts.creation_input_files.create import *
+
+
+parser = argparse.ArgumentParser(description='make input files for convPhy')
+parser.add_argument("-vcf", "--path-to-directory-with-vcf-files", required=True, help="path to dir with vcf files")
+parser.add_argument("-outgroup", "--path-to-outgroup", required=True, help="path to outgroup")
+parser.add_argument("-out-phy", "--name-phylip-file", default="convphy.phy", help="path to outg phylip")
+parser.add_argument("-o", "--path_to_out_directory", default="input", help="path to out directory")
+
+args = parser.parse_args()
+
+os.makedirs(args.path_to_out_directory, exist_ok=True)
+
+write_phylip(args.path_to_directory_with_vcf_files, args.path_to_out_directory, args.name_phylip_file, args.path_to_outgroup)
+create_snps_file(args.path_to_directory_with_vcf_files, args.path_to_out_directory)
+create_info_pos(args.path_to_directory_with_vcf_files, args.path_to_out_directory)
--- scripts/creation_input_files/create.py
+++ scripts/creation_input_files/create.py
@@ -95,20 +95,3 @@ def create_info_pos(path_to_directory_with_vcf_files, path_to_out_directory):
     with open(path_to_out_info_pos, "w") as info_pos_out:
         info_pos_out.writelines(map(lambda line: "\t".join(line), zip(sorted_position, snps, ref)))
 
-
-
-if __name__ == '__main__':
-
-	parser = argparse.ArgumentParser(description='make input files for convPhy')
-	parser.add_argument("-vcf", "--path-to-directory-with-vcf-files", required=True, help="path to dir with vcf files")
-	parser.add_argument("-outgroup", "--path-to-outgroup", required=True, help="path to outgroup")
-	parser.add_argument("-out-phy", "--name-phylip-file", default="convphy.phy", help="path to outg phylip")
-	parser.add_argument("-o", "--path_to_out_directory", default="input", help="path to out directory")
-
-	args = parser.parse_args()
-
-	os.makedirs(args.path_to_out_directory, exist_ok=True)
-
-	write_phylip(args.path_to_directory_with_vcf_files, args.path_to_out_directory, args.name_phylip_file, args.path_to_outgroup)
-	create_snps_file(args.path_to_directory_with_vcf_files, args.path_to_out_directory)
-	create_info_pos(args.path_to_directory_with_vcf_files, args.path_to_out_directory)
